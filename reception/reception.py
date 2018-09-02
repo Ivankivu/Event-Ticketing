fbea6cb4cb3ce278c277c082af45f58d79801f4d
Ivankivu <ivankivu@gmail.com>
2018-09-03 00:56:41 +0300

    modified reception/reception.py css/header.css css/responsive.css css/style.css index.h tml [#160185508]

--- reception/reception.py
+++ reception/reception.py
@@ -1,63 +1,24 @@
-#user string input to search for 
+# user string input to search for
 lines = []
+
 user_input = input('Enter name:')
 
-empty = ''
-with open ('ordinary.txt') as ordinary_file:
+with open('ordinary.txt')as ordinary_file:
     lines = ordinary_file.readlines()
-for line in lines:
-        if user_input =='':
+    for line in lines:
+        if user_input == '':
             user_input = input('Enter name:')
-             
-for line in lines:
-        if user_input.lower() in line.lower():
-            print (line,'ordinary ticket')
-            break
-          
-with open ('vip.txt') as vip_file:           
-    
-        linez = vip_file.readlines()
-        for line in linez:
-            if user_input =='':
-                user_input = input('Enter name:')
-                
-for line in linez:
-        if user_input.lower() in line.lower():
-            print (line, 'vip ticket')
+        elif user_input.lower()in line.lower():
+            print(line, 'ordinary ticket', end = '')
             break
 
-        
-    
-
-
-
-if user_input != line:
-        print ('Not registered', end = '')
-
-
-    
-
-
-
-
-    # for file_input in ordinary_file:
-    #     if user_input is empty:
-    #         user_input = input('Enter name:')
-    #     else:
-
-    #         if user_input.lower() in file_input.lower():
-    #             print (file_input, 'ordinary ticket')
-    # else:
-    #     if user_input is not file_input:
-    #         print ('Not registered')
-    
-    # ordinary_file.close()
-
-    # for file_input in vip_file: 
-    #     if user_input.lower() in file_input.lower():
-    #         print (file_input, 'vip ticket')
-    # else:
-    #     if user_input is not file_input:
-    #         print ('Not registered')
-                
-    # vip_file.close()
+with open('vip.txt')as vip_file:
+    linez = vip_file.readlines()
+    for line in linez:
+        if user_input == '':
+            user_input = input('Enter name:')
+        elif user_input.lower()in line.lower():
+            print(line, 'vip Ticket', end = '')
+            break
+    # if user_input != line:
+    #     print ('Not registered')
