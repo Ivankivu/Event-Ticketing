5932000c9bbc23d00ea8f4b594f62c075f0d0116
Ivankivu <ivankivu@gmail.com>
2018-09-07 12:49:05 +0300

    Creating classes for Uers
    classes to interact with the api to add,retrive and delete user [#160314091]

--- classes/person.py
+++ classes/person.py
@@ -1,16 +1,18 @@
 from humans import Person, User, GuestList, EmptyNameException
+import manage
+from main.api.users import get_user, get_users, add_user, delete_user
 
 person = Person("Ivan Kivumbi","male",26)
 
 print(Person.details())
 
-user = User("kivumbi ivan","bunga-soya",25, "Male")
+user = User("kivumbi ivan","bunga-soya", 25, "Male")
 
 print("This  is {},coming from {}, {} years old and a {}.".format(user.name, user.address, user.age, user.sex))
 
 guestlist = GuestList()
 
 
-added_user = guesslist.find_user_by_email(1)
+saved_users = guestlist.get_users(1)
 
-print(added_user)
+print(saved_users)
